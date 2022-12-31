34a21a69196d6dd521610091e9ff0d09a5b1f5c2
Elahe Soltani <elahesoltani196@gmail.com>
2022-12-31 18:38:38 +0330

    add project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
+import {ApolloClient , ApolloProvider , InMemoryCache} from '@apollo/client'
+import { ThemeProvider } from '@mui/material';
+import theme from './mui/theme'
+import "./styles/fonts.css"
+import "./styles/index.css"
+const client=new ApolloClient({
+  uri:"https://api-ca-central-1.hygraph.com/v2/cl80k2om70b2e01t9hbkdaz5e/master" ,
+  cache:new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+   <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+     <App />
+    </ThemeProvider>  
+   </ApolloProvider>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
